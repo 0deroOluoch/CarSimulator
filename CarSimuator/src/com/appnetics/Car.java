59a0e9c9389f0f875cd00d5a02586465a5727943
Odero Oluoch <52481094+0deroOluoch@users.noreply.github.com>
2020-04-22 04:22:39 +0300

    Add files via upload

---
+++ CarSimuator/src/com/appnetics/Car.java
@@ -0,0 +1,4 @@
+package com.appnetics;
+
+public class Car {
+}
---
+++ CarSimuator/src/com/appnetics/Main.java
@@ -0,0 +1,8 @@
+package com.appnetics;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Launching car simulator ");
+    }
+}
